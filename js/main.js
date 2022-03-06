c897346840425725a4832f6e13bd945c19440b6f
Kiguchi Saki <saki.kiguchi@gmail.com>
2022-03-06 21:11:11 +0900

    myfirstcommit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    $(".nav_btn").on("click",function(){
+      $('.nav_btn').toggleClass('open');
+          $('#headerinner').toggleClass('open');
+      
+    });
+
+  });
+
+function slideSwitch() {
+   var $active = $('#mainvisual img.active');
+
+   if ( $active.length == 0 ) $active = $('#mainvisual img:last');
+
+   var $next =  $active.next().length ? $active.next()
+      : $('#mainvisual img:first');
+
+   $active.addClass('last-active');
+
+   $next.css({opacity: 0.0})
+      .addClass('active')
+      .animate({opacity: 1.0}, 1000, function() {
+           $active.removeClass('active last-active');
+      });
+}
+
+$(function() {
+   setInterval( "slideSwitch()", 3000 );
+});
